971771a207aa66050389d49f141358831283b201
Jason Wiker <jason.wiker@gmail.com>
2021-03-11 21:19:50 -0700

    Add appsync module

---
+++ lambdaSourceAppsync/variables.tf
@@ -0,0 +1,16 @@
+variable "account_id" {}
+variable "region" {
+  default = "us-east-1"
+}
+variable "env" {}
+variable "ecr_name" {}
+variable "image_uri" {}
+
+variable "resolvers" {
+  description = "Map of datasources to create"
+  type        = any
+  default     = {}
+}
+
+variable "name" {}
+variable "schema" {}
