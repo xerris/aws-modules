3d36341a8827a0682b058c726031e7681ce61e61
Dariem Garcés <catdemian@gmail.com>
2021-04-15 18:53:49 -0500

    Update outputs.tf

--- lambdaSourceAppsync/outputs.tf
+++ lambdaSourceAppsync/outputs.tf
@@ -1,4 +1,9 @@
+output "appsync_id" {
+  description = "ID of created appsync"
+  value = aws_appsync_graphql_api.foe_api.id
+}
 
 output "appsync_id" {
+  description = "ID of created appsync"
   value = aws_appsync_graphql_api.foe_api.uris["graphql"]
 }
