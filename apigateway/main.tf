fe615f7072c181a008de38c8b519e6e30bfe00f2
Nelson Lopez <amaya.fabian.n@gmail.com>
2021-07-15 14:42:38 -0600

    CCTMS-62: Fix cycle and variables

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -12,7 +12,7 @@ resource "aws_api_gateway_rest_api" "api-gw" {
 
 resource "aws_api_gateway_resource" "gw-resource" {
   #count = length(var.resources_path_details) > 0 ? length(var.resources_path_details) : 0
-  for_each = var.resources_path_details
+  for_each = {for rs in var.resources_path_details: rs.resource_path => rs }
 
   rest_api_id = aws_api_gateway_rest_api.api-gw.id
   parent_id   = lookup(each.value, "parent_resource", "root") == "root" ? aws_api_gateway_rest_api.api-gw.root_resource_id : aws_api_gateway_resource.gw-resource["${lookup(each.value, "parent_resource", "root")}"]
--- apigateway/variables.tf
+++ apigateway/variables.tf
@@ -10,11 +10,11 @@ variable "apigateway_name" {
   default     = ""
 }
 
-variable "resources_path_details" {
-  description = "Details for your api resources path and http methods"
-  type        = any
-  default     = {}
-}
+#variable "resources_path_details" {
+#  description = "Details for your api resources path and http methods"
+#  type        = any
+#  default     = {}
+#}
 
 #variable "resources_path_details" {
 #  type = map(object({
@@ -29,20 +29,20 @@ variable "resources_path_details" {
 #}
 
 
-#variable "resources_path_details" {
-#  description = "Details for your api resources path and http methods"
-#  type = list(object({
-#    "resources" = {
-#      resource_path    = string
-#      http_method      = string
-#      integration_type = string
-#      integration_uri  = string
-#      lambda_name      = string
-#      status_code      = string
-#      parent_resource  = string
-#    }
-#  }))
-#}
+variable "resources_path_details" {
+  description = "Details for your api resources path and http methods"
+  type = list(object({
+    "resources" = {
+      resource_path    = string
+      http_method      = string
+      integration_type = string
+      integration_uri  = string
+      lambda_name      = string
+      status_code      = string
+      parent_resource  = string
+    }
+  }))
+}
 
 variable "tags" {
   description = "A mapping of tags to assign to all resources"
