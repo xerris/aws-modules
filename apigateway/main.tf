95820812924765c4cea4a8b3e8632887e81b6172
Nelson Lopez <amaya.fabian.n@gmail.com>
2021-07-15 18:44:39 -0600

    CCTMS-62: Fix references

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -35,7 +35,7 @@ resource "aws_api_gateway_integration" "apigw-integration" {
 
   rest_api_id             = aws_api_gateway_rest_api.api-gw.id
   resource_id             = each.value.parent_resource == "root" ? aws_api_gateway_rest_api.api-gw.root_resource_id : aws_api_gateway_resource.gw-resource["${each.value.parent_resource}"].id #aws_api_gateway_resource.gw-resource.*.id[count.index]
-  http_method             = each.value.http_method  #aws_api_gateway_method.gw-method.*.http_method[count.index]
+  http_method             = aws_api_gateway_method.gw-method["${each.value.resource_path}"].http_method #each.value.http_method  #aws_api_gateway_method.gw-method.*.http_method[count.index]
   integration_http_method = each.value.integration_type == "AWS_PROXY" ? "POST" : each.value.http_method
   #integration_http_method = element(var.resources_path_details, count.index).integration_type == "AWS_PROXY" ? "POST" : element(var.resources_path_details, count.index).http_method
   type                    = each.value.integration_type #element(var.resources_path_details, count.index).integration_type
@@ -48,7 +48,7 @@ resource "aws_api_gateway_method_response" "method-response" {
 
   rest_api_id = aws_api_gateway_rest_api.api-gw.id
   resource_id = each.value.parent_resource == "root" ? aws_api_gateway_rest_api.api-gw.root_resource_id : aws_api_gateway_resource.gw-resource["${each.value.parent_resource}"].id #aws_api_gateway_resource.gw-resource.*.id[count.index]
-  http_method = each.value.http_method #aws_api_gateway_method.gw-method.*.http_method[count.index]
+  http_method = aws_api_gateway_method.gw-method["${each.value.resource_path}"].http_method #each.value.http_method #aws_api_gateway_method.gw-method.*.http_method[count.index]
   status_code = each.value.status_code #element(var.resources_path_details, count.index).status_code
 }
 
@@ -58,8 +58,8 @@ resource "aws_api_gateway_integration_response" "integration-response" {
 
   rest_api_id = aws_api_gateway_rest_api.api-gw.id
   resource_id = each.value.parent_resource == "root" ? aws_api_gateway_rest_api.api-gw.root_resource_id : aws_api_gateway_resource.gw-resource["${each.value.parent_resource}"].id #aws_api_gateway_resource.gw-resource.*.id[count.index]
-  http_method = each.value.http_method #aws_api_gateway_method.gw-method.*.http_method[count.index]
-  status_code = each.value.status_code #aws_api_gateway_method_response.method-response.*.status_code[count.index]
+  http_method = aws_api_gateway_method.gw-method["${each.value.resource_path}"].http_method #each.value.http_method #aws_api_gateway_method.gw-method.*.http_method[count.index]
+  status_code = aws_api_gateway_method_response.method-response["${each.value.resource_path}"].status_code #each.value.status_code #aws_api_gateway_method_response.method-response.*.status_code[count.index]
 }
 
 resource "aws_api_gateway_deployment" "default" {
