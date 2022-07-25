1e054b27609ad11d5b7b01e0489deda21cfeb497
aetorres <andres.torresduran@gmail.com>
2022-07-25 11:05:27 -0600

    adding output info to apigatewayLambda

--- apiGatewayLambda/outputs.tf
+++ apiGatewayLambda/outputs.tf
@@ -2,3 +2,7 @@ output "base_url" {
   value       = aws_api_gateway_stage.apistage.invoke_url
   description = "API Gateway Endpoint"
 }
+
+output "api_gateway_id"{
+  value = aws_api_gateway_rest_api.api-gw.id
+}
