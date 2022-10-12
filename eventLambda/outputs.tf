c861c80c2ffa4c5cd324327021364ffd4e131b91
Andres Torres <andres.torresduran@gmail.com>
2022-10-12 17:18:23 -0600

    adding output arn

--- eventLambda/outputs.tf
+++ eventLambda/outputs.tf
@@ -2,3 +2,8 @@ output "this_lambda_arn" {
   description = "ARN of the Lambda function"
   value       = aws_lambda_function.lambda_event.invoke_arn
 }
+
+output "arn" {
+  description = "ARN of the Lambda function"
+  value       = aws_lambda_function.lambda_event.arn
+}
